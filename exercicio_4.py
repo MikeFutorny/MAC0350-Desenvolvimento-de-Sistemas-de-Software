6b8e3123e807caf9f8fd217c970ac6d327ad4b42
Mikhail Futorny <mikhailfutorny@Mikhails-MacBook-Air.local>
2026-03-26 08:49:53 -0300

    exercicio_4

---
+++ exercicio_4.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+#DB em lista
+usuarios = []
+
+class User(BaseModel):
+    nome: str
+    idade: int
+
+
+@app.delete("/users")
+async def delete_users():
+    usuarios.clear()
+    return {"message": "Todos os users foram removidos"}
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    with open("index.html", "r", encoding="utf-8") as f:
+        return f.read()
+
+
+@app.post("/users")
+async def add_user(user: User):
+    usuarios.append(user.dict())
+    return {"message": "user adicionado", "users": usuarios}
+
+@app.get("/users")
+async def get_users(index: int | None = None):
+    if index is not None:
+        if 0 <= index < len(usuarios):
+            return usuarios[index]
+        return {"error": "indice invalido"}
+    return usuarios
